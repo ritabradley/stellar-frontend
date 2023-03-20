6a3fe639d88fd8f89c402d590374f28097736447
Rita Bradley <rita@ritabradley.dev>
2023-03-20 11:31:53 -0700

    :heavy_plus_sign: add `react-parallax-tilt`

---
+++ src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import Tilt from 'react-parallax-tilt';
+
+const Logo = () => {
+    return (
+        <Tilt>
+            <div>
+                <h1 className='text-2xl font-bold m-4 mt-0'>Stellar</h1>
+            </div>
+        </Tilt>
+    );
+};
+
+export default Logo;

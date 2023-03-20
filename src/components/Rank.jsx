4f0ca359675b813874040fc2afb265d883a244b3
Rita Bradley <rita@ritabradley.dev>
2023-03-20 13:26:42 -0700

    :lipstick: change text apperance
    - make text a white-ish color
    - enclose the number of entries in a span
    - make number of entries bold and a larger font size

--- src/components/Rank.jsx
+++ src/components/Rank.jsx
@@ -1,8 +1,8 @@
 const Rank = ({ name = 'Rita', entries = 7 }) => {
     return (
         <div>
-            <p>
-                {name}, you've submitted {entries} images for detection
+            <p className='text-xl text-violet-50'>
+                {name}, you've submitted <span className='text-2xl font-bold'>{entries} </span>images for detection
             </p>
         </div>
     );

ecabcfebe4bd75bf1c100c0dbb0678bb72fe0598
Rita Bradley <rita@ritabradley.dev>
2023-03-20 12:18:39 -0700

    :lipstick: add shooting start image as logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,17 +1,18 @@
 import Tilt from 'react-parallax-tilt';
+import star from '../assets/shooting-star.png';
 
 const Logo = () => {
     return (
         <Tilt
-            className='bg-main-gradient rounded shadow-md'
+            className='bg-main-gradient rounded shadow-md p-6'
             perspective={500}
             glareEnable={true}
             glareMaxOpacity={0.45}
             scale={1.02}
             style={{ height: 150, width: 150 }}
         >
-            <div className='inner-element'>
-                <h1 className='text-2xl font-bold m-4 mt-0'>Stellar</h1>
+            <div className='inner-element '>
+                <img src={star} />
             </div>
         </Tilt>
     );

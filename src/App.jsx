ea0c62560bff75705c9873fdb7a269e5f7ebf2fa
Rita Bradley <rita@ritabradley.dev>
2023-03-20 15:03:30 -0700

    :lipstick: add cobweb particles to background

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import Navigation from './components/Navigation';
 import Logo from './components/Logo';
 import Rank from './components/Rank';
 import ImageForm from './components/ImageForm';
+import ParticlesBg from 'particles-bg';
 
 const App = () => {
     return (
         <div>
+            <ParticlesBg color='#F5F3FF' num={250} type='cobweb' bg={true} />
             <Navigation />
             <Logo />
             <div className='grid place-content-center text-center font-mono'>

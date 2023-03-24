f10e12daca94bf2b92aef1214c97867faa40f8fc
Rita Bradley <rita@ritabradley.dev>
2023-03-23 17:00:37 -0700

    :sparkles: add bounding boxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,95 @@
-import Navigation from './components/Navigation';
-import Logo from './components/Logo';
-import Rank from './components/Rank';
-import ImageForm from './components/ImageForm';
-import FaceRecognition from './components/FaceRecognition';
-import ParticlesBg from 'particles-bg';
+import {useState} from "react";
+import Navigation from "./components/Navigation";
+import Logo from "./components/Logo";
+import Rank from "./components/Rank";
+import ImageForm from "./components/ImageForm";
+import FaceRecognition from "./components/FaceRecognition";
+import ParticlesBg from "particles-bg";
 
 const App = () => {
+	const [imageBytes, setImageBytes] = useState("");
+	const [faceData, setFaceData] = useState([]);
 
+	const handleImageUpload = async (file) => {
+		try {
+			const reader = new FileReader();
+			reader.readAsDataURL(file);
+			reader.onload = () => {
+				const base64Image = reader.result.split(",")[1];
+				setImageBytes(base64Image);
+				// Call Clarifai API with base64Image here
 
-    return (
-        <div className='font-mono'>
-            <ParticlesBg color='#F5F3FF' num={250} type='cobweb' bg={true} />
-            <Navigation />
-            <Logo />
-            <div className='grid place-content-center text-center w-full max-w-4xl mt-6 mx-auto'>
-                <Rank />
-                <ImageForm />
-                <FaceRecognition />
-            </div>
-        </div>
-    );
+				const PAT = "9e8702498a2b42b38c254d821f529f5a";
+				const USER_ID = "e8pb750e3eqt";
+				const APP_ID = "stellar-face-finder";
+				const MODEL_ID = "face-detection";
+				const MODEL_VERSION_ID = "6dc7e46bc9124c5c8824be4822abe105";
+				const raw = JSON.stringify({
+					"user_app_id": {
+						"user_id": USER_ID,
+						"app_id": APP_ID
+					},
+					"inputs": [
+						{
+							"data": {
+								"image": {
+									"base64": base64Image
+								}
+							}
+						}
+					]
+				});
+				const requestOptions = {
+					method: "POST",
+					headers: {
+						"Accept": "application/json",
+						"Authorization": "Key " + PAT
+					},
+					body: raw
+				};
+				fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+					.then(response => response.json())
+					.then(result => {
+						const faceRegions = result.outputs[0].data.regions;
+						const imageElement = document.getElementById("input-image");
+						const width = Number(imageElement.width);
+						const height = Number(imageElement.height);
+						const faceData = faceRegions.map(region => {
+							const boundingBox = region.region_info.bounding_box;
+							return {
+								topRow: boundingBox.top_row * height,
+								rightCol: width - boundingBox.right_col * width,
+								bottomRow: height - boundingBox.bottom_row * height,
+								leftCol: boundingBox.left_col * width,
+							};
+						});
+						handleFaceData(faceData);
+					})
+					.catch(error => console.log("error", error));
+			};
+		} catch (error) {
+			console.error("Error uploading image:", error);
+		}
+	};
+
+	const handleFaceData = (data) => {
+		setFaceData(data);
+	};
+
+	return (
+		<div className="font-mono">
+			<ParticlesBg color="#F5F3FF" num={250} type="cobweb" bg={true}/>
+			<Navigation/>
+			<Logo/>
+			<div
+				className="grid place-content-center text-center w-full max-w-4xl mt-6 mx-auto">
+				<Rank/>
+				<ImageForm onImageUpload={handleImageUpload}/>
+				<FaceRecognition imageBytes={imageBytes} faceData={faceData}/>
+
+			</div>
+		</div>
+	);
 };
 
 export default App;
--- src/components/FaceRecognition.jsx
+++ src/components/FaceRecognition.jsx
@@ -1,8 +1,33 @@
-// FaceRecognition.jsx
-const FaceRecognition = ({ imageUrl }) => {
+import React from 'react';
+
+const FaceRecognition = ({ imageBytes, faceData }) => {
+    const displayFaceBoxes = () => {
+        return faceData.map((face, index) => {
+            const { topRow, rightCol, bottomRow, leftCol } = face;
+            return (
+                <div
+                    key={index}
+                    className='absolute border-2 border-violet-500'
+                    style={{
+                        top: topRow,
+                        right: rightCol,
+                        bottom: bottomRow,
+                        left: leftCol,
+                    }}
+                ></div>
+            );
+        });
+    };
+
     return (
-        <div className='relative'>
-            Face Recognition
+        <div className='relative w-full'>
+            <img
+                id='input-image'
+                className='w-full h-full object-fit'
+                src={`data:image/jpeg;base64,${imageBytes}`}
+                alt='Uploaded'
+            />
+            {displayFaceBoxes()}
         </div>
     );
 };
